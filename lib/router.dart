89b730d368d2430ec47d50c1cfc7e8a4248657b7
admin-at-519398779771 <eyesh0011@gmail.com>
2024-07-20 16:23:34 +0900

    signout

--- lib/router.dart
+++ lib/router.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final routerProvider = Provider((ref) {
+  ref.watch(authState);
   return GoRouter(
       initialLocation: SignUpOrSignIn.routeURL,
       redirect: (context, state) {
